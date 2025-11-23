4f0c7acddccd748eb6e37f7a1f4c71b32fd47d22
entric96-max <entric96@gmail.com>
2025-11-23 18:24:31 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -5,13 +5,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCuiZHU3m2un_wWQH4ztefffUWKjhFMOXc",
-  authDomain: "aiinterview-12f78.firebaseapp.com",
-  projectId: "aiinterview-12f78",
-  storageBucket: "aiinterview-12f78.firebasestorage.app",
-  messagingSenderId: "962054449606",
-  appId: "1:962054449606:web:e597a18d9ed37fdd772ac7",
-  measurementId: "G-C9LBSJPV78"
+
 };
 
 // Initialize Firebase
